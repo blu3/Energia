02d151d42c2e4604b4ae57fec659aa9f248eb3c9
Rick Kimball <rick@kimballsoftware.com>
2012-11-01 14:30:31 -0400

    add support for array versions of new and delete

--- hardware/lm4f/cores/lm4f/new.cpp
+++ hardware/lm4f/cores/lm4f/new.cpp
@@ -8,7 +8,15 @@ void * operator new(size_t size)
 void operator delete(void * ptr)
 {
   free(ptr);
-} 
+}
+
+void *operator new[](size_t size) {
+  return malloc(size);
+}
+
+void operator delete[](void *p) {
+  free(p);
+}
 
 int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
 void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
